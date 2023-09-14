aa7b23a1c6cd1038244c3095bec6e474ddf0b24d
kkCJ <102225702+kai112013@users.noreply.github.com>
2023-09-14 08:07:39 -0300

    Add files via upload
    contribuição

---
+++ perfilN.js
@@ -0,0 +1,9 @@
+window.alert("Bem vindo Moreno!");
+
+//document.getElementById("clica").window.print;//
+
+function myFunction() {
+    document.getElementById("string1").innerHTML = "Neguin Games na area";
+    document.getElementById("string2").innerHTML = "Sim o Brabo  dos mundos dos games";
+    document.getElementById("string3").innerHTML = "Busque na maior rede de videos da internet";
+}
